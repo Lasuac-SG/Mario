7ff0099ba2c91dd60b70a213926d78d97171f5e5
youngthen <3240105586@zju.edu.cn>
2026-07-09 16:39:28 +0800

    chore: Cmake工程所需文件
    这部分需要配置好vcpkg可以导入外部库, 看到输出Hello, Mario!

---
+++ src/precompile.h
@@ -0,0 +1,19 @@
+// pre-compile inlcude header files
+
+#ifndef MARIO_PRECOMPILE_H
+#define MARIO_PRECOMPILE_H
+
+#include <cassert>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cmath>
+
+#include <SFML/Graphics.hpp>
+
+#endif  // MARIO_PRECOMPILE_H
