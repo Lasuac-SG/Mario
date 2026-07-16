523708fedd6c462b128dd67c2828227462b094c2
youngthen <3240105586@zju.edu.cn>
2026-07-16 16:44:18 +0800

    fix(common) TileType 新类型

--- src/common/EventId.h
+++ src/common/EventId.h
@@ -2,7 +2,7 @@
 #define MARIO_EVENTID_H
 
 enum class MarioState { IDLE, RUNNING, JUMPING, FALLING, DEAD };
-enum class TileType { EMPTY = 0, GROUND = 1, PIPE = 2, PLATFORM = 3 };
+enum class TileType { EMPTY = 0, GROUND = 1, PIPE = 2, PLATFORM = 3, QUESTION, BRICK};
 enum class Direction { LEFT, RIGHT };
 enum class ModelEvent : uint32_t { STATE_CHANGED = 0, };
 enum class ViewModelEvent : uint32_t { RENDER_UPDATE = 0, };
